d49e8cb37306ba5bbdf1245df4a2556882ffa483
dikaizm <dikamaah@gmail.com>
2023-05-31 16:30:42 +0700

    add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Stelar - Digital Agency</title>
 
@@ -300,9 +300,9 @@
       <div class="flex flex-col px-6 py-20 mx-auto h-fit max-w-7xl gap-y-10 lg:px-8">
         <div class="flex flex-col items-center text-center gap-y-6">
           <h2 class="text-5xl font-bold">Explore what we have done</h2>
-        <p class="max-w-2xl leading-loose text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. </p>
+          <p class="max-w-2xl leading-loose text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
 
         <div class="grid justify-between gap-8 grid-auto-fit-xl">
@@ -361,157 +361,111 @@
 
         <div class="flex flex-col items-center mt-4">
           <a href="#"
-          class="px-4 py-2 text-xs font-semibold transition-all duration-200 border rounded-full w-fit border-sky-700 text-sky-700 hover:bg-sky-700 hover:text-white">SEE
-          OUR WORKS</a>
+            class="px-4 py-2 text-xs font-semibold transition-all duration-200 border rounded-full w-fit border-sky-700 text-sky-700 hover:bg-sky-700 hover:text-white">SEE
+            OUR WORKS</a>
         </div>
       </div>
     </section>
 
     {{-- Testimonial section --}}
-    {{-- <section class="relative mt-32">
-      <div class="px-6 mx-auto max-w-7xl lg:px-8">
-        <h2 class="text-5xl font-bold">What clients are saying about our services</h2>
+    <section class="relative mt-20 bg-neutral-900">
+      <div class="px-6 py-16 mx-auto max-w-7xl lg:px-8">
+        <div class="flex justify-between gap-x-4">
+          <img class="object-contain" src="assets/testimonial-01.png" alt="">
 
-        <div class="grid gap-8 mt-12 grid-auto-fit-lg">
-          <div class="p-5 transition-all duration-200 bg-white border rounded-lg hover:drop-shadow-xl">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas.
-            </p>
-            <div class="flex items-center mt-6 gap-x-3">
-              <img class="object-cover w-10 h-10 rounded-full"  src="https://images.unsplash.com/photo-1584611139207-9cf4fed64188?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="">
-              <div class="flex flex-col gap-y-1">
-                <p class="text-sm font-semibold">Name</p>
-                <p class="text-xs text-neutral-400">Desc</p>
+          <div class="flex flex-col px-10 gap-y-6">
+            <svg class="w-12 text-white h-fit" width="currentWidth" height="currentHeight" viewBox="0 0 42 34"
+              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M4.99995 33.7999C3.72691 33.7999 2.50601 33.2942 1.60584 32.394C0.705664 31.4939 0.199951 30.273 0.199951 28.9999L0.199949 12.9999C0.199949 9.60516 1.54852 6.34943 3.94898 3.94896C6.34945 1.5485 9.60518 0.199927 12.9999 0.199927L12.9999 3.39993C10.4539 3.39993 8.01207 4.41135 6.21172 6.2117C4.41137 8.01205 3.39995 10.4538 3.39995 12.9999L3.39995 14.5999L14.5999 14.5999C15.873 14.5999 17.0939 15.1056 17.9941 16.0058C18.8942 16.906 19.3999 18.1269 19.3999 19.3999L19.3999 28.9999C19.3999 30.273 18.8942 31.4939 17.9941 32.394C17.0939 33.2942 15.873 33.7999 14.6 33.7999L4.99995 33.7999ZM27.4 33.7999C26.1269 33.7999 24.906 33.2942 24.0058 32.394C23.1057 31.4939 22.5999 30.273 22.5999 28.9999L22.5999 12.9999C22.5999 9.60515 23.9485 6.34943 26.349 3.94896C28.7494 1.5485 32.0052 0.199926 35.3999 0.199925L35.3999 3.39993C32.8539 3.39993 30.4121 4.41135 28.6117 6.2117C26.8114 8.01205 25.7999 10.4538 25.7999 12.9999L25.7999 14.5999L36.9999 14.5999C38.273 14.5999 39.4939 15.1056 40.3941 16.0058C41.2942 16.906 41.7999 18.1269 41.7999 19.3999L41.7999 28.9999C41.7999 30.273 41.2942 31.4939 40.3941 32.394C39.4939 33.2942 38.273 33.7999 37 33.7999L27.4 33.7999Z" />
+            </svg>
+            <p class="text-2xl leading-relaxed text-white">I really enjoyed working with you guys, you guys are very
+              communicative and quick to do
+              the job. I really can't wait to work with you again!</p>
+            <div class="flex items-center mt-4 gap-x-5">
+              <img class="object-cover w-16 h-16 rounded-full"
+                src="https://images.unsplash.com/photo-1584611139207-9cf4fed64188?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+                alt="">
+              <div class="flex flex-col">
+                <p class="text-xl font-semibold text-white">Stefani Mila</p>
+                <p class="text-lg text-neutral-400">CEO, Brainware</p>
               </div>
             </div>
           </div>
-          <div class="p-5 transition-all duration-200 bg-white border rounded-lg hover:drop-shadow-xl">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas.
-            </p>
-            <div class="flex items-center mt-6 gap-x-3">
-              <img class="object-cover w-10 h-10 rounded-full"  src="https://images.unsplash.com/photo-1584611139207-9cf4fed64188?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="">
-              <div class="flex flex-col gap-y-1">
-                <p class="text-sm font-semibold">Name</p>
-                <p class="text-xs text-neutral-400">Desc</p>
-              </div>
-            </div>
-          </div>
-          <div class="p-5 transition-all duration-200 bg-white border rounded-lg hover:drop-shadow-xl">
-            <p class="text-sm ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas.
-            </p>
-            <div class="flex items-center mt-6 gap-x-3">
-              <img class="object-cover w-10 h-10 rounded-full"  src="https://images.unsplash.com/photo-1584611139207-9cf4fed64188?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="">
-              <div class="flex flex-col gap-y-1">
-                <p class="text-sm font-semibold">Name</p>
-                <p class="text-xs text-neutral-400">Desc</p>
-              </div>
-            </div>
+
+          <div class="flex flex-col self-center gap-y-4">
+            <svg class="w-2 text-white h-fit" width="currentWidth" height="currentHeight" viewBox="0 0 8 8"
+              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="4" cy="4" r="4" />
+            </svg>
+            <svg class="w-2 h-fit text-neutral-600" width="currentWidth" height="currentHeight" viewBox="0 0 8 8"
+              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="4" cy="4" r="4" />
+            </svg>
+            <svg class="w-2 h-fit text-neutral-600" width="currentWidth" height="currentHeight" viewBox="0 0 8 8"
+              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="4" cy="4" r="4" />
+            </svg>
           </div>
         </div>
       </div>
-    </section> --}}
- 
+    </section>
   </main>
 
   {{-- Footer --}}
-  <footer class="relative mt-32 bg-neutral-900">
+  <footer class="relative w-full bg-neutral-900">
+    <div class="px-6 pb-12 mx-auto lg:px-12 max-w-[89rem]">
+      <div class="pb-8 rounded-3xl bg-sky-700 px-14 pt-14">
+        <section class="flex justify-between">
+          <p class="text-4xl text-white">Have an awesome project together?</p>
+          <a href="" class="px-6 py-3 text-white border rounded-full">START A PROJECT</a>
+        </section>
 
-  </footer>
+        <div class="mt-12 mb-10 h-[1px] bg-white/40"></div>
 
-  {{-- Footer --}}
-  <footer class="mt-40 bg-white dark:bg-gray-900">
-    <div class="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
-      <div class="md:flex md:justify-between">
-        <div class="mb-6 md:mb-0">
-          <a href="https://flowbite.com/" class="flex items-center">
-            <img src="https://flowbite.com/docs/assets/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-          </a>
-        </div>
-        <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-            <ul class="font-medium text-gray-600 dark:text-gray-400">
-              <li class="mb-4">
-                <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-              </li>
-              <li>
-                <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-              </li>
-            </ul>
+        <section class="flex justify-between">
+          <div class="flex flex-col max-w-lg gap-8 text-sm">
+            <img class="h-10 w-fit" src="assets/stelar-logo-white.svg" alt="">
+            <p class="leading-loose text-white">Let us help you blast off into the stratosphere with a
+              stunning and functional design that will engage and captivate your audience.</p>
+            <div>
+              <p class="text-white">hello@stelarhub.com</p>
+            <p class="text-white">(+62) 8560-0080-9354</p>
+            </div>
           </div>
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-            <ul class="font-medium text-gray-600 dark:text-gray-400">
-              <li class="mb-4">
-                <a href="https://github.com/themesberg/flowbite" class="hover:underline">Github</a>
-              </li>
-              <li>
-                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-              </li>
-            </ul>
+          <div class="flex gap-20 text-white whitespace-nowrap">
+            <div class="flex flex-col gap-6">
+              <p class="font-bold">Site</p>
+              <ul class="flex flex-col gap-3 text-sm">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Platform</a></li>
+                <li><a href="">Solutions</a></li>
+                <li><a href="">Learn</a></li>
+              </ul>
+            </div>
+
+            <div class="flex flex-col gap-6">
+              <p class="font-bold">Support</p>
+              <ul class="flex flex-col gap-3 text-sm">
+                <li><a href="">FAQ</a></li>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Terms of Service</a></li>
+              </ul>
+            </div>
+
+            <div class="flex flex-col gap-6">
+              <p class="font-bold">Social Media</p>
+              <ul class="flex flex-col gap-3 text-sm">
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Linkedin</a></li> 
+              </ul>
+            </div>
           </div>
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-            <ul class="font-medium text-gray-600 dark:text-gray-400">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <hr class="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2023 <a href="https://flowbite.com/"
-            class="hover:underline">Flowbite™</a>. All Rights Reserved.
-        </span>
-        <div class="flex mt-4 space-x-6 sm:mt-0 sm:justify-center">
-          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd"
-                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                clip-rule="evenodd" />
-            </svg>
-            <span class="sr-only">Facebook page</span>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd"
-                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                clip-rule="evenodd" />
-            </svg>
-            <span class="sr-only">Instagram page</span>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path
-                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-            </svg>
-            <span class="sr-only">Twitter page</span>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                clip-rule="evenodd" />
-            </svg>
-            <span class="sr-only">GitHub account</span>
-          </a>
-          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-              <path fill-rule="evenodd"
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                clip-rule="evenodd" />
-            </svg>
-            <span class="sr-only">Dribbble account</span>
-          </a>
-        </div>
+        </section>
+
+        <p class="mt-8 text-sm text-center text-white">PT Semesta Teknologi Karya © 2023 - Jakarta</p>
       </div>
     </div>
   </footer>
