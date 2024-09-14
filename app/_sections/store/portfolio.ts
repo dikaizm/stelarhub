8120807281e176d94ed93e421d184b0408ff6006
dikaizm <dikamaah@gmail.com>
2024-09-14 22:23:10 +0700

    feat: add satudna portfolio

--- app/_sections/store/portfolio.ts
+++ app/_sections/store/portfolio.ts
@@ -1,21 +1,28 @@
 import { CardPortfolioProps } from "../PortfolioSection";
 
+import satuDnaImg from '@/assets/images/portfolio/satudna.jpeg'
 import fiveElementsImg from '@/assets/images/portfolio/five-elements.png'
 import mataFutsalImg from '@/assets/images/portfolio/matafutsal.png'
 import optiGuardImg from '@/assets/images/portfolio/optiguard.jpeg'
 
 export const portfolioData: CardPortfolioProps[] = [
   {
-    title: "5Elements",
-    description: "Website yang menghadirkan informasi lengkap tentang profil perusahaan dan sarana promosi yang efektif. Dengan tampilan desain yang unik dan menarik, setiap elemen visual mencerminkan esensi dan nilai-nilai perusahaan, menciptakan pengalaman yang mengesankan bagi pengunjung.",
-    tags: ['Website', 'Event Organizer', 'UI/UX Design', 'Frontend Development'],
-    img: fiveElementsImg
+    title: "SatuDNA by SATUSEHAT Mobile",
+    description: "SatuDNA merupakan portal layanan kesehatan berbasis DNA yang memberikan informasi kesehatan personal berdasarkan analisis DNA. Pengguna dapat mendaftar sebagai partisipan, mendapatkan hasil analisis DNA, dan melihat personalisasi kesehatan berdasarkan genomik.",
+    tags: ['Mobile App', 'Healthcare', 'UI/UX Design'],
+    img: satuDnaImg
   },
   {
     title: "MataFutsal",
     description: "MataFutsal hadir sebagai website untuk menemukan dan memesan lapangan futsal dengan mudah. Di landing page, pengguna dapat menemukan informasi tentang lokasi, fasilitas, harga, lengkap dengan promosi terkini. Terdapat dashboard khusus admin untuk manajemen booking yang efisien dan simpel.",
     tags: ['Website', 'Futsal', 'UI/UX Design', 'Frontend Development'],
     img: mataFutsalImg
+  },
+  {
+    title: "5Elements",
+    description: "Website yang menghadirkan informasi lengkap tentang profil perusahaan dan sarana promosi yang efektif. Dengan tampilan desain yang unik dan menarik, setiap elemen visual mencerminkan esensi dan nilai-nilai perusahaan, menciptakan pengalaman yang mengesankan bagi pengunjung.",
+    tags: ['Website', 'Event Organizer', 'UI/UX Design', 'Frontend Development'],
+    img: fiveElementsImg
   },
   {
     title: "OptiGuard",
